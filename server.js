ac4c09a48297b3aa36998dc8e0189f4da6eab942
ankit-jha-dev <ankit.kumar@oditeksolutions.com>
2025-08-22 14:29:12 +0530

    Update server port to 3002

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 // Middleware
 app.use(cors());
